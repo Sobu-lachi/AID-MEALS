488aa43158dad5c990f5dc2da4fe2a7986269189
Sobulachi <sobula07@gmail.com>
2026-09-16 12:17:10 +0100

    Use of Zod to handle runtime errors

--- Server/apis/home.ts
+++ Server/apis/home.ts
@@ -1,25 +1,32 @@
 import express from 'express'
-import { type Response, type Request } from 'express';
+import z from 'zod';
 
 const homeRouter = express.Router();
 
-interface bodyData{
-    email: string,
-    password: string
-}
+const bodyData= z.object({
+    email: z.email(),
+    password: z.string().min(6)
+})
 
-interface apiResponse{
-    success: boolean,
-    message: string
-}
+type LoginBody = z.infer<typeof bodyData>
 
-homeRouter.post('/home', async (req:Request<{},apiResponse, bodyData>, res:Response<apiResponse>)=>{
-    const {email, password} = req.body;
+
+homeRouter.post('/home', async (req, res)=>{
+    const result = bodyData.safeParse(req.body);
+
+    if (!result.success) {
+        return res.status(400).json({
+            success: false,
+            message: result.error 
+        });
+    }
+
+    const { email, password } = result.data; 
 
     res.json({
         success: true,
-        message: 'Heyyyy'
-    })
+        message: `Welcome back, ${email}!`
+    });
 
 })
 
